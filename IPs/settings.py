756b5183dfbbba2c48265884598ea9f9bf432e5a
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-13 18:48:24 +0300

    Added home route and templates folder

--- IPs/settings.py
+++ IPs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallary.apps.GallaryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- gallary/views.py
+++ gallary/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to my Gallary')
+def home(request):
+
+    return render(request, 'home.html')
+
+
