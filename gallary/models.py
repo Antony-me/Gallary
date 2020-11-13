8722aab27c261f6dd3ac1a3388585b00966a814f
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-13 22:37:49 +0300

    Added tests

--- gallary/models.py
+++ gallary/models.py
@@ -40,7 +40,6 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=60)
     description = models.TextField()
-    author = models.CharField(max_length=40, default='admin')
     date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category,  on_delete = models.CASCADE)
     location = models.ForeignKey(Location,  on_delete = models.CASCADE)
