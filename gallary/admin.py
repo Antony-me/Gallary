9a72e5b4a7424acb7a09c6d35ee0e207c4c42bec
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-13 22:20:07 +0300

    Added models logic

--- gallary/admin.py
+++ gallary/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Cartegory, Location, Photo
+from .models import Category, Location, Image
 
 # Register your models here.
 
 admin.site.register(Location)
-admin.site.register(Cartegory)
-admin.site.register(Photo)
+admin.site.register(Category)
+admin.site.register(Image)
--- gallary/views.py
+++ gallary/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Photo
+from .models import Image
 
 # Create your views here.
 def home(request):
