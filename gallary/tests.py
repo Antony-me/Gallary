02ecf8469e74932586175c22806e9dd6e900ebbc
Antony Simiyu <antonymunyasia993@gmail.com>
2020-11-13 22:50:16 +0300

    Added docstrings to tests

--- gallary/tests.py
+++ gallary/tests.py
@@ -19,16 +19,29 @@ class TestImage(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.image_test, Image))
-
+ 
+    '''
+     Test to test save image method
+    ''' 
+    
     def test_save_image(self):
         self.image_test.save_image()
         after = Image.objects.all()
         self.assertTrue(len(after) > 0)
 
+
+    '''
+     Test to test delete image method
+    ''' 
+
     def test_delete_image(self):
         self.image_test.delete_image()
         images = Image.objects.all()
         self.assertTrue(len(images) == 0)
+
+    '''
+     Test to test update image method
+    ''' 
 
     def test_update_image(self):
         self.image_test.save_image()
@@ -36,20 +49,49 @@ class TestImage(TestCase):
         changed_img = Image.objects.filter(image='photos/test.jpg')
         self.assertTrue(len(changed_img) > 0)
 
+    '''
+     Test to test get-image-by id  method
+    ''' 
+
     def test_get_image_by_id(self):
         found_image = self.image_test.get_image_by_id(self.image_test.id)
         image = Image.objects.filter(id=self.image_test.id)
         self.assertTrue(found_image, image)
+
+    '''
+     Test to test filter_image_by_location method
+    ''' 
+
 
     def test_search_image_by_location(self):
         self.image_test.save_image()
         found_images = self.image_test.filter_by_location(location='Nairobi')
         self.assertTrue(len(found_images) == 1)
 
+
+    '''
+     Test to test search image by category method
+    ''' 
+
     def test_search_image_by_category(self):
         category = 'Game'
         found_img = self.image_test.search_by_category(category)
         self.assertTrue(len(found_img) > 1)
+    self.assertTrue(found_image, image)
+
+    '''
+     Test to test filter_image_by_location method
+    ''' 
+
+
+    def test_search_image_by_location(self):
+        self.image_test.save_image()
+        found_images = self.image_test.filter_by_location(location='Nairobi')
+        self.assertTrue(len(found_images) == 1)
+
+    '''
+     Tear Down
+    '''
 
     def tearDown(self):
         Image.objects.all().delete()
@@ -58,6 +100,10 @@ class TestImage(TestCase):
 
 
 class TestLocation(TestCase):
+    '''
+    setup Method
+    '''
+
     def setUp(self):
         self.location = Location(name='Nairobi')
         self.location.save_location()
@@ -65,21 +111,41 @@ class TestLocation(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.location, Location))
 
+    '''
+    Test to test save_location method
+    '''
+
     def test_save_location(self):
         self.location.save_location()
         locations = Location.get_locations()
         self.assertTrue(len(locations) > 0)
+
+    
+    '''
+    Test to test get_location method
+    '''
 
     def test_get_locations(self):
         self.location.save_location()
         locations = Location.get_locations()
         self.assertTrue(len(locations) > 1)
 
+
+    
+    '''
+    Test to test Update_location method
+    '''
+
     def test_update_location(self):
         new_location = 'Nowhere'
         self.location.update_location(self.location.id, new_location)
         changed_location = Location.objects.filter(name='Nowhere')
         self.assertTrue(len(changed_location) > 0)
+
+    
+    '''
+    Test to test delete_location method
+    '''
 
     def test_delete_location(self):
         self.location.delete_location()
@@ -88,6 +154,11 @@ class TestLocation(TestCase):
 
 
 class CategoryTestClass(TestCase):
+
+    '''
+    SetUp method
+    '''
+
     def setUp(self):
         self.category = Category(name='Game')
         self.category.save_category()
@@ -95,10 +166,19 @@ class CategoryTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.category, Category))
 
+    
+    '''
+    Test to test save_category method
+    '''
+
     def test_save_category(self):
         self.category.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
+
+    '''
+    Test to test delete_category method
+    '''
 
     def test_delete_category(self):
         self.category.delete_category()
